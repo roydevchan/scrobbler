web-scrobbler/web-scrobbler

Delta Radio UK Connector

core/connectors.js
@@ -1131,7 +1131,16 @@ define(function () {
 			],
 		js: ['connectors/v2/radioplayer.js'],
 		version: 2
-	}];
+	},
+
+	{
+		label: 'Delta Radio UK',
+		matches: ['*://deltaradiouk.co.uk/popout-player*'],
+		js: ['connectors/v2/deltaradiouk.js'],
+		version: 2
+	},];
+
+
 
 
 });

roydevchan/scrobbler

core/connectors.js
@@ -1066,5 +1066,10 @@ define(function () {
 		version: 2
 	},
 
-	];
+	{
+		label: 'Delta Radio UK',
+		matches: ['*://deltaradiouk.co.uk/popout-player*'],
+		js: ['connectors/v2/deltaradio.js'],
+		version: 2
+	}];
 });